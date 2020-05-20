2e80b291f6502ca8b14d62a77273a3a662399ed2
Marvin <Marvin@DESKTOP-412F5VK.fritz.box>
2020-05-20 09:47:13 +0200

    rechnen methode und test

--- jenkinstest/src/main/java/jenkinstest/Rechner.java
+++ jenkinstest/src/main/java/jenkinstest/Rechner.java
@@ -1,5 +1,8 @@
 package jenkinstest;
 
 public class Rechner {
-
+		
+	public int rechnen(int number, int number2) {
+		return number + number2;
+	}
 }
---
+++ jenkinstest/src/test/java/jenkinstest/rechnentest.java
@@ -0,0 +1,18 @@
+package jenkinstest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class rechnentest {
+
+	@Test
+	public void test() {
+		Rechner rechner = new Rechner();
+		
+		int result = rechner.rechnen(10, 89);
+		
+		assertEquals(99, result);
+	}
+
+}
